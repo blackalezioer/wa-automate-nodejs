eb5f97e998b066fb662f20d65c485fd12afb0585
blackalezioer <76170289+blackalezioer@users.noreply.github.com>
2021-01-28 17:42:38 +0530

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,25 @@
 const meow = require('meow');
-const wa = require('../dist');
+const wa = require('@open-wa/wa-automate');
+
+wa.create({
+  sessionId: "COVID_HELPER",
+  authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+  blockCrashLogs: true,
+  disableSpins: true,
+  headless: true,
+  hostNotificationLang: 'PT_BR',
+  logConsole: false,
+  popup: true,
+  qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+}).then(client => start(client));
+
+function start(client) {
+  client.onMessage(async message => {
+    if (message.body === 'Hi') {
+      await client.sendText(message.from, '👋 Hello!');
+    }
+  });
+}
 const { create, SimpleListener, generatePostmanJson, ev } = wa;
 const path = require('path');
 const express = require('express');
